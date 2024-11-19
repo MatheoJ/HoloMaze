c5f876d291c26a843617a5b49a602086512dd524
HugoFoucrit <hugo.foucrit@gmail.com>
2024-11-19 12:41:57 -0500

    restart lorsque touché par araignée

---
+++ Assets/Script/AI/SpiderCollider.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpiderCollider : MonoBehaviour
+{
+    private Vector3 spawnPoint;
+    private void Start()
+    {
+        spawnPoint = transform.position;
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.name == "SpiderEnemy")
+        {
+            transform.position = spawnPoint;
+        }
+    }
+}
